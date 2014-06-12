39ef26a6e9cb8e1adbe30fe24e1b82f7fe07cc5c
stevenlee87 <imepisode1@gmail.com>
2014-06-12 16:08:22 +0800

    views upgrade

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import *
-from mysite.views import hello, current_datetime
+from mysite.views import hello, current_datetime, hours_ahead
 
 urlpatterns = patterns('',
     ('^hello/$', hello),
     ('^time/$', current_datetime),
+    ('^time/plus/(\d{1,2})$', hours_ahead),
 )
