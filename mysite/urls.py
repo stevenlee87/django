bcb847ca822d91054b5357f0e12c2236cb68606a
stevenlee87 <imepisode1@gmail.com>
2014-06-26 19:03:04 +0800

    add django admin

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import *
 from mysite.views import hello, current_datetime, hours_ahead
+from django.contrib import admin
+admin.autodiscover()
+
 
 urlpatterns = patterns('',
     ('^hello/$', hello),
     ('^time/$', current_datetime),
     ('^time/plus/(\d{1,2})/$', hours_ahead),
+	('^admin/', include(admin.site.urls)),
 )
