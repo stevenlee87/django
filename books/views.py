b3ebca935c7c4811eaefab001354bcfd12ac33db
stevenlee87 <imepisode1@gmail.com>
2014-07-16 18:29:01 +0800

    add contact

--- books/views.py
+++ books/views.py
@@ -1,13 +1,33 @@
 from django.http import HttpResponse 
 from django.shortcuts import render_to_response
+from books.models import Book
 
-def search_form(request):
-    return render_to_response('search_form.html')
+#def search_form(request):
+#    return render_to_response('search_form.html')
+
+#def search(request):
+#    if 'q' in request.GET:
+#        message = 'You searched for: %r' % request.GET['q']
+#    else:
+#        message = 'You submitted an empty form.'
+#    return HttpResponse(message)
+
+#def search(request):
+#    if 'q' in request.GET and request.GET['q']:
+#        print request.GET
+#        q = request.GET['q']
+#        books = Book.objects.filter(title__icontains=q)
+#        return render_to_response('search_results.html', {'books': books, 'query': q})
+#    else:
+#        return HttpResponse('Please submit a search term.')
 
 def search(request):
+    error = False
     if 'q' in request.GET:
-        message = 'You searched for: %r' % request.GET['q']
-    else:
-        message = 'You submitted an empty form.'
-    return HttpResponse(message)
-
+        q = request.GET['q']
+        if not q:
+            error = True
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render_to_response('search_results.html', {'books': books, 'query': q})
+    return render_to_response('search_form.html', {'error': error})
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import *
 from mysite.views import hello, current_datetime, hours_ahead, current_url_view_good, display_meta
 #from mysite.views import *
 from django.contrib import admin
-from books import views
+from books import views as bookviews
+from contact import views as contactviews
 admin.autodiscover()
 
 
@@ -13,6 +14,7 @@ urlpatterns = patterns('',
     ('^admin/', include(admin.site.urls)),
 	('^current/$', current_url_view_good),
 	('^display_meta/$', display_meta),
-	('^search-form/$', views.search_form),
-	('^search/$', views.search),
+	#('^search-form/$', views.search_form),
+	('^search/$', bookviews.search),
+	('^contact/$', contactviews.contact),
 )
